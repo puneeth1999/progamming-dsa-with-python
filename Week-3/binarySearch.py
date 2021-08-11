f36cd179ed38e1978b9030f8167ea57f73abb74f
puneeth1999 <puneethchoppanati@gmail.com>
2021-08-11 12:39:26 +0530

    Added some notes and selection sort

--- Week-3/binarySearch.py
+++ Week-3/binarySearch.py
@@ -1,3 +1,22 @@
+'''
+Notes on BINARY SEARCH:
+
+=> Check if the middle element of the current array is the element you're searching for. If yes, return.
+=> Else, there are two scenarios.
+	1. The target element is greater than the middle element.
+	2. The target element is lesser than the middle element.
+=> Scenario 1 [target > middle]:
+	=> Return the function with modified parameters. Let it only search the slice from middle+1 till end
+=> Scenario 2 [target < middle]:
+	=> Return the function with modified parameters. Let it only search the slice from the start to middle.
+
+=> Here, we can see that the slices are cut into half everytime we fail to find the element i.e., every iteration.
+   So...
+
+   Time Complexity  = O(log N)
+   Space Complexity = O(1) Since we aren't using any additional space.
+'''
+
 def binarySearch(seq, start, end, target): 
 	# seq is the array/list; start and end are the bounds and target is that value to search for
 	# Please note that the array -  seq must be in sorted in non-decreasing order for this version of Binary search to work
---
+++ Week-3/selectionSort.py
@@ -0,0 +1,29 @@
+'''
+Notes on SELECTION SORT:
+=> Scan the list from left to right
+	=> During the scan, perform the following:
+	=> Set min_pos to the current position.
+	=> Now, scan again using another loop from the current position to the right of the list. Let's call this a slice.
+		=> If the present element within the slice is less than min_pos element, l[min_pos] = l[present]
+	=> At this point, the minimum value of the current slice is found.
+	=> Now, swap the minimum value with the current position
+=> Return the list.
+
+Time Complexity	: O(N) where N is the size of the input
+Space Complexity: O(1) since, it does not take up any extra space
+'''
+
+def selectionSort(l):
+	# Scans from left to right
+	for start in range(len(l)):
+		min_pos = start # Gets assigned to the current position of the pointer
+		# Starts searching for min value from the current pointer (start) position
+		for i in range(start, len(l)):
+			if (l[i] < l[min_pos]):
+				min_pos = i
+		# Swap
+		l[min_pos], l[start] = l[start], l[min_pos]
+	return l
+
+if __name__ == "__main__":
+	print(selectionSort([7,6,4,5,9,2,0,1]))
