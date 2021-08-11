de03470cd3c867a95063da9ab9d4b4006bbfc397
puneeth1999 <puneethchoppanati@gmail.com>
2021-08-11 14:46:12 +0530

    addded insertion sort

---
+++ Week-3/insertionSort.py
@@ -0,0 +1,18 @@
+'''
+Notes on INSERTION SORT:
+
+The code is pretty self-explanatory
+
+Time Complexity  = O(N)
+Space Complexity = O(1)
+'''
+
+def insertionSort(l):
+	for pointer in range(len(l)):
+		pos = pointer
+		while (pos > 0 and l[pos] < l[pos - 1]):
+			l[pos], l[pos - 1] = l[pos - 1], l[pos]
+			pos -= 1
+	return l
+if __name__ == "__main__":
+	print(insertionSort([7,6,4,5,9,2,0,1]))
